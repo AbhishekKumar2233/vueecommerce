24412f54f952e6a2def85fb4f241d8aa403f8a86
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2021-12-31 00:39:14 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <VmProductsList></VmProductsList>
+  </div>
+</template>
+
+<script>
+import VmProductsList from "@/components/products_list/ProductsListContainer";
+
+export default {
+  name: "index",
+  components: {
+    VmProductsList,
+  },
+};
+</script>
